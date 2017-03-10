2a211fdd20c5b91f29ad1e31d3e588cae3da0df3
Daniel da Silva <daniel@developmentseed.org>
2017-03-10 13:15:10 +0000

    Add chart to section mobilidade reduzida

--- app/assets/scripts/reducers/national.js
+++ app/assets/scripts/reducers/national.js
@@ -19,7 +19,7 @@ export default function reducer (state = initialState, action) {
       if (action.error) {
         state.error = action.error;
       } else {
-        state.data = processData(action.data);
+        state.data = processData(action.data.results);
       }
       break;
   }
@@ -31,10 +31,20 @@ function processData (distritos) {
 
   // Licenças and max per district.
   distritos = distritos.map(d => {
+    d.data.licencas2006 = d.data['lic-geral'][0].value + d.data['lic-mob-reduzida'][0].value;
     d.data.licencas2016 = _.last(d.data['lic-geral']).value + _.last(d.data['lic-mob-reduzida']).value;
     d.data.max2016 = _.last(d.data['max-lic-geral']).value + _.last(d.data['max-lic-mob-reduzida']).value;
     return d;
   });
+
+  // Total licenças 2006.
+  data.licencas2006 = _.sumBy(distritos, 'data.licencas2006');
+
+  // Total licenças mob reduzida 2006.
+  data.licencasMobReduzida2006 = _.sumBy(distritos, d => d.data['lic-mob-reduzida'][0].value);
+
+  // Total licenças mob reduzida 2016.
+  data.licencasMobReduzida2016 = _.sumBy(distritos, d => _.last(d.data['lic-mob-reduzida']).value);
 
   // Total licenças 2016.
   data.licencas2016 = _.sumBy(distritos, 'data.licencas2016');
@@ -50,9 +60,10 @@ function processData (distritos) {
 
   data.totalMunicipios = _.sumBy(distritos, d => d.concelhos.length);
 
+  // Number of municípios with lic-mob-reduzida
   data.totalMunicipiosMobReduzida = _.sumBy(distritos, d => d.concelhos.filter(o => {
     if (!o.data['lic-mob-reduzida']) {
-      console.error(`Concelho: ${o.name} doesn't have data`);
+      console.error(`Concelho: ${o.name} doesn't have data on "lic-mob-reduzida"`);
       return false;
     }
     return _.last(o.data['lic-mob-reduzida']).value !== 0;
