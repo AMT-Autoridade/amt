ad2ad552f812f2ede2a4d36cf09b888cc4d2de90
leihla <leihla@gmail.com>
2017-03-20 23:29:16 +0000

    Add copy for info pages

--- app/assets/scripts/components/sections/section-sobre.js
+++ app/assets/scripts/components/sections/section-sobre.js
@@ -8,13 +8,17 @@ var SectionSobre = React.createClass({
 
   render: function () {
     return (
-      <div id='section-sobre' className='container-wrapper'>
+      <div id='section-sobre' className='container-wrapper section-wrapper'>
+        
+        <h1>Sobre o Projeto</h1>
+
         <section id='intro'>
-          <h1>Sobre o Projeto</h1>
-          <p>Num momento em que discute o enquadramento regulatório do transporte de passageiros em veículos ligeiros, considera-se prioritário reforçar e promover o conhecimento sobre o sector, nomeadamente sobre os serviços de transporte em táxi.</p>
+          <div>
+            <p className='lead'>A Autoridade da Mobilidade e dos Transportes identificou como uma prioridade estratégica o desenvolvimento e a implementação do Observatório da Mobilidade, como uma ferramenta essencial à compreensão, definição e aplicação de uma regulação eficiente no Ecossistema da Mobilidade e dos Transportes.</p>
 
+            <p className='lead'>A recolha estatística apresentada visa dar a conhecer factos essenciais sobre as condições de prestação de serviços de táxi, sendo o conhecimento resultante desta análise constitui um esforço proactivo que possibilita uma avaliação adequada dos efeitos das opções legislativas e regulatórias que venham a ser tomadas quanto ao transporte de passageiros em veículos ligeiros.</p>
+          </div>
           
-
         </section>
       </div>
     );
