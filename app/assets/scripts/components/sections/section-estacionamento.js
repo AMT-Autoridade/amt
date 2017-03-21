c256ea717287d161fe804d540c86c7b4fcc72080
leihla <leihla@gmail.com>
2017-03-21 15:53:12 +0000

    Add css styles to estacionamento section

--- app/assets/scripts/components/sections/section-estacionamento.js
+++ app/assets/scripts/components/sections/section-estacionamento.js
@@ -13,31 +13,9 @@ var SectionEstacionamento = React.createClass({
           <header className='section-header'>
             <h3>Portugal</h3>
             <h1>Regime de Estacionamento</h1>
-            <p>As câmaras municipais estabelecem os regimes de estacionamento de táxis que se aplicam no seu concelho. Estas disposições são estabelecidas por regulamento municipal ou aquando da atribuição da licença municipal ao veículo.</p>
+            <p className='lead'>As câmaras municipais estabelecem os regimes de estacionamento de táxis que se aplicam no seu concelho. Estas disposições são estabelecidas por regulamento municipal ou aquando da atribuição da licença municipal ao veículo.</p>
           </header>
           <div className='section-content'>
-            <ul className='section-stats two-columns'>
-              <li>
-                <span className='stat-number'>F</span>
-                <span className='stat-description'><strong>Fixo:</strong> obrigados a estacionar em locais determinados e constantes da licença.</span>
-              </li>
-              <li>
-                <span className='stat-number'>C</span>
-                <span className='stat-description'><strong>Condicionado:</strong> podem estacionar em locais reservados para o efeito, até ao limite fixado.</span>
-              </li>
-              <li>
-                <span className='stat-number'>L</span>
-                <span className='stat-description'><strong>Livre:</strong> podem circular livremente, não existindo locais obrigatórios para estacionamento.</span>
-              </li>
-              <li>
-                <span className='stat-number'>E</span>
-                <span className='stat-description'><strong>Escala:</strong> cumprem um regime sequencial de prestação de serviço</span>
-              </li>
-            </ul>
-
-            { /*
-            // Graph goes here
-           */}
            <div className='two-columns'>
              <div className='graph'>
               <p><strong>Graph goes here</strong> Percentagem de municípios por regime de estacionamento</p>
--- app/assets/scripts/views/app.js
+++ app/assets/scripts/views/app.js
@@ -17,18 +17,22 @@ var App = React.createClass({
       <div className={c('page', pageClass)}>
         <header id="page-header">
           <h1 id='page-logo'><a href="#">AMT</a></h1>
-          <ul className='primary-nav'>
-            <li><a href="/#/#licenças">Licenças</a></li>
-            <li><a href="/#/glossario">Glossário</a></li>
-            <li><a href="#">Relatório</a></li>
-            <li><a href="/#/dados">Dados</a></li>
-          </ul>
-          <ul className='secondary-nav'>
-            <li><a href="/#/sobre">Sobre</a></li>
-            <li><a href="/#/glossario">Glossário</a></li>
-            <li><a href="#">Relatório</a></li>
-            <li><a href="/#/dados">Dados</a></li>
-          </ul>
+          <nav className='page-nav'>
+            <ul className='primary-nav'>
+              <li><a href="/#/#section-licencas">Licenças</a></li>
+              <li><a href="/#/section-mobilidade">Mobilidade Reduzida</a></li>
+              <li><a href="#">Estacionamento</a></li>
+              <li><a href="#">Distribuição</a></li>
+              <li><a href="#">Evolução</a></li> 
+              <li><a href="#">Conclusões</a></li>
+            </ul>
+            <ul className='secondary-nav'>
+              <li><a href="/#/sobre">Sobre</a></li>
+              <li><a href="/#/glossario">Glossário</a></li>
+              <li><a href="#">Relatório</a></li>
+              <li><a href="/#/dados">Dados</a></li>
+            </ul>
+          </nav>
         </header>
         <main className='page__body' role='main'>
           {this.props.children}
