e94ba509e555a5675b9d2a6b22b18ccbec958512
Daniel da Silva <daniel@developmentseed.org>
2017-03-31 00:15:30 +0100

    Add dynamic share link

--- app/assets/scripts/components/sections/section-intro.js
+++ app/assets/scripts/components/sections/section-intro.js
@@ -15,10 +15,10 @@ var SectionIntro = React.createClass({
           <p className='lead'>Num momento em que discute o enquadramento regulatório do transporte de passageiros em veículos ligeiros, considera-se prioritário reforçar e promover o conhecimento sobre o sector, nomeadamente sobre os serviços de transporte em táxi.</p>
 
           <ul className='social-share'>
-            <li><a className='facebook' href="https://www.facebook.com/sharer/sharer.php?u=https%3A//leihla.github.io/amt/%23/" title="Partilhar no Facebook"><span className='i-facebook'></span></a></li>
-            <li><a className='linkedin' href="https://www.linkedin.com/shareArticle?mini=true&url=https%3A//leihla.github.io/amt/%23/&title=Observat%C3%B3rio%20da%20Mobilidade%20e%20dos%20Transportes%20&summary=&source=" title="Partilhar no LinkedIn"><span className='i-linkedin2'></span></a></li>
-            <li><a className='twitter' href="https://twitter.com/home?status=https%3A//leihla.github.io/amt/%23/" title="Partilhar no Twitter"><span className='i-twitter'></span></a></li>
-            <li><a className='googleplus' href="https://plus.google.com/share?url=https%3A//leihla.github.io/amt/%23/" title="Partilhar no Google+"><span className='i-google-plus'></span></a></li>
+            <li><a className='facebook' href={`https://www.facebook.com/sharer/sharer.php?u=${window.location.href}`} title="Partilhar no Facebook"><span className='i-facebook'></span></a></li>
+            <li><a className='linkedin' href={`https://www.linkedin.com/shareArticle?mini=true&title=Observat%C3%B3rio%20da%20Mobilidade%20e%20dos%20Transportes%20&url=${window.location.href}`} title="Partilhar no LinkedIn"><span className='i-linkedin2'></span></a></li>
+            <li><a className='twitter' href={`https://twitter.com/home?status=${window.location.href}`} title="Partilhar no Twitter"><span className='i-twitter'></span></a></li>
+            <li><a className='googleplus' href={`https://plus.google.com/share?url=${window.location.href}`} title="Partilhar no Google+"><span className='i-google-plus'></span></a></li>
           </ul>
 
         </section>
