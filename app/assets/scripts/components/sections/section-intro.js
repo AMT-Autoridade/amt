c21469da921e944a28f966be18515643d623f5cf
leihla <leihla@gmail.com>
2017-04-03 11:13:40 +0100

    Fix section intro layout

--- app/assets/scripts/components/sections/section-intro.js
+++ app/assets/scripts/components/sections/section-intro.js
@@ -8,20 +8,22 @@ var SectionIntro = React.createClass({
 
   render: function () {
     return (
-      <div id='intro-wrapper' className='container-wrapper'>
-        <section id='intro' className='section-wrapper'>
-          <h3 className='section-category'>Observatório dos Mercados da Mobilidade,<span className='block'> Preços e Estratégias Empresariais</span></h3>
-          <h1>Táxis em Portugal 2006-2016</h1>
-          <p className='lead'>Num momento em que discute o enquadramento regulatório do transporte de passageiros em veículos ligeiros, considera-se prioritário reforçar e promover o conhecimento sobre o sector, nomeadamente sobre os serviços de transporte em táxi.</p>
+      <div id='intro-wrapper'>
+        <div id='intro' className='container-wrapper content-wrapper'>
+          <section >
+            <h3 className='section-category'>Observatório dos Mercados da Mobilidade,<span className='block'> Preços e Estratégias Empresariais</span></h3>
+            <h1>Táxis em Portugal 2006-2016</h1>
+            <p className='lead'>Num momento em que discute o enquadramento regulatório do transporte de passageiros em veículos ligeiros, considera-se prioritário reforçar e promover o conhecimento sobre o sector, nomeadamente sobre os serviços de transporte em táxi.</p>
 
-          <ul className='social-share'>
-            <li><a className='facebook' href={`https://www.facebook.com/sharer/sharer.php?u=${window.location.href}`} title="Partilhar no Facebook"><span className='i-facebook'></span></a></li>
-            <li><a className='linkedin' href={`https://www.linkedin.com/shareArticle?mini=true&title=Observat%C3%B3rio%20da%20Mobilidade%20e%20dos%20Transportes%20&url=${window.location.href}`} title="Partilhar no LinkedIn"><span className='i-linkedin2'></span></a></li>
-            <li><a className='twitter' href={`https://twitter.com/home?status=${window.location.href}`} title="Partilhar no Twitter"><span className='i-twitter'></span></a></li>
-            <li><a className='googleplus' href={`https://plus.google.com/share?url=${window.location.href}`} title="Partilhar no Google+"><span className='i-google-plus'></span></a></li>
-          </ul>
+            <ul className='social-share'>
+              <li><a className='facebook' href={`https://www.facebook.com/sharer/sharer.php?u=${window.location.href}`} title="Partilhar no Facebook"><span className='i-facebook'></span></a></li>
+              <li><a className='linkedin' href={`https://www.linkedin.com/shareArticle?mini=true&title=Observat%C3%B3rio%20da%20Mobilidade%20e%20dos%20Transportes%20&url=${window.location.href}`} title="Partilhar no LinkedIn"><span className='i-linkedin2'></span></a></li>
+              <li><a className='twitter' href={`https://twitter.com/home?status=${window.location.href}`} title="Partilhar no Twitter"><span className='i-twitter'></span></a></li>
+              <li><a className='googleplus' href={`https://plus.google.com/share?url=${window.location.href}`} title="Partilhar no Google+"><span className='i-google-plus'></span></a></li>
+            </ul>
 
-        </section>
+          </section>
+        </div>
       </div>
     );
   }
