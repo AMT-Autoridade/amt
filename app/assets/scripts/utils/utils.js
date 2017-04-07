94d5256d6e7d20f17562d1f4b5f0b9f94a1d81a5
Daniel da Silva <daniel@developmentseed.org>
2017-04-07 20:03:15 +0100

    Fix round

--- app/assets/scripts/utils/utils.js
+++ app/assets/scripts/utils/utils.js
@@ -1,12 +1,12 @@
 'use strict';
 export function percent (value, total, decimals = 2) {
   let val = value / total * 100;
-  if (decimals === 0) {
-    decimals = val < 1 ? 1 : 0;
-  }
   return round(val, decimals);
 }
 
 export function round (value, decimals = 2) {
+  if (decimals === 0) {
+    decimals = value < 1 ? 1 : 0;
+  }
   return Math.round(value * Math.pow(10, decimals)) / Math.pow(10, decimals);
 }
