3ea9067edc9e5aa8dc25018f0dba4b1c42069b56
Daniel da Silva <daniel@developmentseed.org>
2017-04-30 17:24:55 +0100

    Add links to dados

--- app/assets/scripts/config/production.js
+++ app/assets/scripts/config/production.js
@@ -4,5 +4,8 @@
  */
 module.exports = {
   environment: 'production',
-  api: 'https://raw.githubusercontent.com/olafveerman/taxis'
+  api: 'https://raw.githubusercontent.com/olafveerman/taxis',
+  // From https://rawgit.com/
+  // Needed to force the download of text files.
+  rawGitApi: 'https://cdn.rawgit.com/olafveerman/taxis'
 };
--- app/assets/scripts/views/dados.js
+++ app/assets/scripts/views/dados.js
@@ -2,6 +2,8 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
+import config from '../config';
+
 var Dados = React.createClass({
   propTypes: {
   },
@@ -15,7 +17,7 @@ var Dados = React.createClass({
         <section id='dados' className='content-wrapper'>
         <h1>Sobre os dados</h1>
           <div>
-            <p className='lead'>Este site contém dados sobre o número de táxis licenciados pelos municípios, os contingentes estabelecidos (número máximo de táxis a licenciar) e as disposições relativas ao seu estacionamento. Os dados referem-se ao período entre 2006 e 2016 e foram recolhidos, pela AMT, junto dos 308 municípios portugueses. De forma a complementar os dados recolhidos e concretizar uma análise mais detalhada, foram também utilizados dados sobre a população residente e sobre as dormidas em estabelecimentos hoteleiros, disponibilizados pelo <a href="http://www.ine.pt" title="Ir para INE" target="_blank">Instituto Nacional de Estatística</a> na sua base de dados pública.</p> 
+            <p className='lead'>Este site contém dados sobre o número de táxis licenciados pelos municípios, os contingentes estabelecidos (número máximo de táxis a licenciar) e as disposições relativas ao seu estacionamento. Os dados referem-se ao período entre 2006 e 2016 e foram recolhidos, pela AMT, junto dos 308 municípios portugueses. De forma a complementar os dados recolhidos e concretizar uma análise mais detalhada, foram também utilizados dados sobre a população residente e sobre as dormidas em estabelecimentos hoteleiros, disponibilizados pelo <a href="http://www.ine.pt" title="Ir para INE" target="_blank">Instituto Nacional de Estatística</a> na sua base de dados pública.</p>
           </div>
           <div>
             <h3>Agradecimentos</h3>
@@ -23,6 +25,14 @@ var Dados = React.createClass({
 
             <h3>Tratamento de Dados</h3>
             <p>Existem dados relativos a 2016 para todos os concelhos. Nos anos anteriores a 2016, para um número marginal de concelhos, não foi possível disponibilizar informação. Para esses concelhos, para os anos em que não existia informação, os valores assumidos na análise decorrem da aplicação, aos dados disponíveis, das taxas de variação anual apuradas para os concelhos com informação.</p>
+
+            <ul>
+              <li><a href={`${config.rawGitApi}/master/data/taxis.csv`} title='Descarregar dados'>taxis</a></li>
+              <li><a href={`${config.rawGitApi}/master/data/dormidas.csv`} title='Descarregar dados'>dormidas</a></li>
+              <li><a href={`${config.rawGitApi}/master/data/population.csv`} title='Descarregar dados'>população</a></li>
+              <li><a href={`${config.rawGitApi}/master/data/area-metadata.csv`} title='Descarregar dados'>contingentes</a></li>
+            </ul>
+
           </div>
         </section>
       </div>
