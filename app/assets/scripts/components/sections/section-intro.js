83c48a196dbb8c150a016b8da0ff97c3a33823bb
Daniel da Silva <daniel@developmentseed.org>
2019-07-15 21:07:31 +0100

    Encode share link

--- app/assets/scripts/components/sections/section-intro.js
+++ app/assets/scripts/components/sections/section-intro.js
@@ -1,13 +1,9 @@
 'use strict';
-import React, { PropTypes as T } from 'react';
+import React from 'react';
 
 import { startYear, endYear } from '../../config';
 
 var SectionIntro = React.createClass({
-  propTypes: {
-    data: T.object
-  },
-
   render: function () {
     return (
       <div id='intro-wrapper'>
@@ -16,14 +12,28 @@ var SectionIntro = React.createClass({
             <h2 id='intro-logo'><a href='#'>Autoridade da Mobilidade e dos Transportes</a></h2>
             <h1>Táxis em Portugal <span className='block'>{startYear}&ndash;{endYear}</span></h1>
             <h3 className='section-category'>Observatório dos Mercados da Mobilidade,<span className='block'> Preços e Estratégias Empresariais</span></h3>
-
             <ul className='social-share'>
-              <li><a className='facebook' href={`https://www.facebook.com/sharer/sharer.php?u=${window.location.href}`} title='Partilhar no Facebook'><span className='i-facebook'></span></a></li>
-              <li><a className='linkedin' href={`https://www.linkedin.com/shareArticle?mini=true&title=Observat%C3%B3rio%20da%20Mobilidade%20e%20dos%20Transportes%20&url=${window.location.href}`} title='Partilhar no LinkedIn'><span className='i-linkedin2'></span></a></li>
-              <li><a className='twitter' href={`https://twitter.com/home?status=${window.location.href}`} title='Partilhar no Twitter'><span className='i-twitter'></span></a></li>
-              <li><a className='googleplus' href={`https://plus.google.com/share?url=${window.location.href}`} title='Partilhar no Google+'><span className='i-google-plus'></span></a></li>
+              <li>
+                <a className='facebook' href={`https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(window.location.href)}`} title='Partilhar no Facebook'>
+                  <span className='i-facebook'></span>
+                </a>
+              </li>
+              <li>
+                <a className='linkedin' href={`https://www.linkedin.com/shareArticle?mini=true&title=Observat%C3%B3rio%20da%20Mobilidade%20e%20dos%20Transportes%20&url=${encodeURIComponent(window.location.href)}`} title='Partilhar no LinkedIn'>
+                  <span className='i-linkedin2'></span>
+                </a>
+              </li>
+              <li>
+                <a className='twitter' href={`https://twitter.com/home?status=${encodeURIComponent(window.location.href)}`} title='Partilhar no Twitter'>
+                  <span className='i-twitter'></span>
+                </a>
+              </li>
+              <li>
+                <a className='googleplus' href={`https://plus.google.com/share?url=${encodeURIComponent(window.location.href)}`} title='Partilhar no Google+'>
+                  <span className='i-google-plus'></span>
+                </a>
+              </li>
             </ul>
-
           </section>
         </div>
       </div>
